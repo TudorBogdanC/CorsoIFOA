e83aa2e9d30f41a84a5a26c657a357f243aeef59
“TudorBogdanC” <toriatteteo@gmail.com>
2023-10-31 16:58:07 +0100

    Esercizio2

--- Settimana2/Lezione1Esercizio1/assets/JavaScript/D1.js
+++ Settimana2/Lezione1Esercizio1/assets/JavaScript/D1.js
@@ -33,12 +33,16 @@ Oggetto
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
 let myName = 'Tudor';
 console.log(myName);
+//document.getElementById('myName').innerHTML += myName 
+
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
+
 console.log(12+20);
 
 
@@ -54,6 +58,7 @@ let nuovoNumero1 = 12;
 let nuovoNumero2 = 20;
 let nuovaSomma = nuovoNumero1 + nuovoNumero2;
 console.log(nuovaSomma);
+
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 /* ESERCIZIO 4
@@ -61,9 +66,12 @@ console.log(nuovaSomma);
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+
 let a = 12;
 let x = a;
 console.log(x);
+
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
@@ -82,11 +90,12 @@ console.log(myName);
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-
+/*
 let numero3 = 4;
 
 let sottrazione = 4 - x;
 console.log(sottrazione);
+*/
 
 
 /* ESERCIZIO 7
@@ -94,9 +103,19 @@ console.log(sottrazione);
  Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
+
 let name1 = 'john';
 let name2 = 'John';
 console.log (name1 === name2);
 
-console.log (name1 === name2.toLowerCase() );
+console.log (name1 === name2.toLowerCase() ); 
+
+
+/*
+let name1 = 'john';
+let name2 = 'John';
+let verifica = name1 === name2;
+console.log(verifica);
+*/
+
 /* SCRIVI QUI LA TUA RISPOSTA */
